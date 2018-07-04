2efaf89049b75e310cce72f11a337fdcd654ee8e
Herwindyo Purbo Kusumo <purbo.kusumo@acommerce.asia>
2018-07-04 11:27:03 +0700

    remove un-necessary code in pub sub architecture

--- app/tasks.py
+++ app/tasks.py
@@ -2,23 +2,6 @@ from config.celery import app
 from celery.schedules import crontab
 from celery.task import periodic_task
 from config.log import logger
-import celery_pubsub
-
-
-@app.task()
-def main_publisher():
-    # pub sub celery. scenario : pub and all sub is one code, sharing same redis db for queue
-    # the publisher and consumer tasks
-    for x in range(0, 100000):
-        add_together.delay(x, 2)
-
-
-@app.task()
-def add_together(a, b):
-    # pub sub celery. scenario : pub and all sub is one code, sharing same redis db for queue
-    # the consumer tasks
-    logger.info("{} + {} = {}".format(a, b, a+b))
-    return a + b
 
 
 @periodic_task(
@@ -38,17 +21,10 @@ def add(*args, **kwargs):
     return args[0]
 
 
-# celery_pubsub.subscribe('some.topic', add)
-
-
 @app.task(
     name="pub_sub_main"
 )
 def main_publisher_distributed():
     for x in range(0, 100000):
-        # res = celery_pubsub.publish('some.topic', x)
         res = add.delay(x)
         logger.info("[publisher 1] {}".format(x))
-        # not working
-        # res = celery_pubsub.publish('some.topic', x)
-        # logger.info("[publisher] {}".format(res))
